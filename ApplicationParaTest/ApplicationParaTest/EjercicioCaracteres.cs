c5ef1a2d5d60160f3d6040743f584b7e56f82ddd
cedricchristoph <cedric@icecraft.es>
2022-02-13 18:39:16 +0000

    .

---
+++ ApplicationParaTest/ApplicationParaTest/EjercicioCaracteres.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationParaTest
+{
+    public class EjercicioCaracteres
+    {
+        /// <summary>
+        /// EXERCICIO CARACTERES PRA TEST
+        /// </summary>
+        public static void Main(string[] args)
+        {
+
+            String resultado = "";
+            String texto = "";
+
+            texto = leerArchivo();
+            if (texto.Equals("")) Console.WriteLine("\nEl programa tiene problemas con el archivo!Vuelve a intentarlo mas tarde");
+            else
+            {
+
+                resultado = transformaTexto(texto);
+
+                Console.WriteLine(resultado);
+
+
+                //contamos el numero de vocales por separado mediante el diccionario
+                recuentoVocales(resultado);
+
+
+            }
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// 
+        /// Metodo que transforma la cadena de caracteres sin espacios, acentos, en minúsculas,
+        /// dada la vuelta y con las vocales en mayúsculas
+        /// </summary>
+        /// <param name="texto"></param>
+        /// <returns></returns>
+        public static string transformaTexto(string texto)
+        {
+
+            String result = "";
+            //eliminar espacios en blanco
+            result = texto.Replace(" ", "");
+
+            //darle la vuelta a la cadena
+            result = darVuelta(result);
+
+            //pasarlo todo a minusculas , así pasar a mayuscula las vocales es mas fácil
+            result = result.ToLower();
+
+            //sacar accents
+            result = sacarAcentos(result);
+
+            //en este momento tenemos la cadena sin espacios, acentos, en minúsculas y dada la vuelta
+
+            //pasamos las vocales a mayúsculas
+            result = result.Replace("a", "A").Replace("e", "E").Replace("i", "I").Replace("o", "O").Replace("u", "U");
+
+            return result;
+
+        }
+
+        /// <summary>
+        /// este método nos proporciona cuantas  a, e, i, o, u tenemos en la frase una vez transformada
+        /// </summary>
+        /// <param name="result"> Cadena que contiene el fichero. No tiene espacios, ni accentos,... </param>
+        public static void recuentoVocales(String result)
+        {
+
+            Dictionary<string, int> contadores = new Dictionary<string, int>();
+            int posicion;
+            //iniciamos el  diccionario a -1
+            contadores["A"] = -1;
+            contadores["E"] = -1;
+            contadores["I"] = -1;
+            contadores["O"] = -1;
+            contadores["U"] = -1;
+
+            //recorremos el diccionario lletra a lletra (keys)
+            //ojo, para que sobreescriba el diccionario debemos añadir el ToList que ves debajo!!
+            foreach (var item in contadores.Keys.ToList())
+            {
+                posicion = -1;
+
+                do
+                {
+                    posicion = result.IndexOf(item, posicion + 1);
+                    contadores[item]++;
+
+                } while (posicion != -1);
+            }
+
+
+
+            foreach (var key in contadores.Keys)
+            {
+                Console.WriteLine("Tenemos {1} - {0}", key, contadores[key]);
+                Console.WriteLine();
+            }
+        }
+
+        /// <summary>
+        /// Este método quita los acentos que existen en catalán/castellano 
+        /// </summary>
+        /// <param name="result">Cadena que contiene el archivo. Result no tiene espacios, está dada la vuelta y en minúsculas</param>
+        /// <returns></returns>
+        public static string sacarAcentos(string result)
+        {
+            char[] vocalesA = { 'à', 'á', 'è', 'é', 'í', 'ò', 'ó', 'ú', 'ï', 'ü' };
+            char[] vocales = { 'a', 'a', 'e', 'e', 'i', 'o', 'o', 'u', 'i', 'u' };
+            for (int i = 0; i < vocales.Length; i++)
+            {
+                result = result.Replace(vocalesA[i], vocales[i]);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Método que da una cadena le da la vuelta
+        /// </summary>
+        /// <param name="result">Cadena que contiene el archivo. Result no contiene espacios</param>
+        /// <returns></returns>
+        public static string darVuelta(string result)
+        {
+            char[] charsOfResult = new char[100];//supongamos que el archivo no tiene demasiados caracteres!!!
+
+            charsOfResult = result.ToCharArray();
+            char[] reversed = charsOfResult.Reverse().ToArray();
+            result = new string(reversed);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Método que lee el archivo y lo pasa a una cadena
+        /// </summary>
+        /// <returns>  La cadena que contiene el archivo </returns>
+        public static String leerArchivo()
+        {
+            String result = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader("frases.txt"))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        result += line;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("The file could not be read!");
+                // Console.WriteLine(e.Message);
+            }
+            return result;
+        }
+    }
+}
---
+++ ApplicationParaTest/ProjectTests/EjercicioCaracteresTest.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ApplicationParaTest;
+
+namespace ProjectTests
+{
+    [TestClass]
+    public class EjercicioCaracteresTest
+    {
+        /// <summary>
+        /// darVuelta
+        /// 
+        /// Queremos probar el correcto funcionamiento del metodo darVuelta, este método
+        /// deberá invertir el orden de la cadena de texto que le proporcionemos.
+        /// 
+        /// Introduciremos la cadena 'abcd'
+        /// 
+        /// La función deberá devolver la cadena 'dcba'
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void darVueltaTest()
+        {
+            string original = "abcd";
+            string esperada = "dcba";
+            string recibida = EjercicioCaracteres.darVuelta(original);
+            Console.WriteLine("----- TEST darVuelta. \nCadena entrada: " + original + "\nSalida esperada: " + esperada + "\nSalida REAL: " + recibida + "\n----- TEST FINALIZADO");
+            Assert.AreEqual(esperada, recibida);
+        }
+
+        /// <summary>
+        /// sacarAcentos
+        /// 
+        /// Queremos probar el correcto funcionamiento de la función de sacarAcentos. La
+        /// función deberá eliminar todo tipo de acentos de la cadena de texto que proporcionemos.
+        /// 
+        /// Introducimos la cadena 'áàéèíìóòúùäöüâêîôû'
+        /// 
+        /// La función deberá devolver la cadena 'aaeeiioouuaouaeiou'
+        /// </summary>
+        [TestMethod]
+        public void sacarAcentosTest()
+        {
+            string original = "áàéèíìóòúùäöüâêîôû";
+            string esperada = "aaeeiioouuaouaeiou";
+            string recibida = EjercicioCaracteres.sacarAcentos(original); 
+            Console.WriteLine("----- TEST sacarAcentos. \nCadena entrada: " + original + "\nSalida esperada: " + esperada + "\nSalida REAL: " + recibida + "\n----- TEST FINALIZADO");
+            Assert.AreEqual(esperada, recibida);
+        }
+
+        /// <summary>
+        /// transformaTexto
+        /// 
+        /// Queremos asegurarnos del correcto comportamiento de la función de transformaTexto.
+        /// Esta deberá recibir una cadena de texto y elminar de ella los espacios, acentos y además
+        /// invertir el orden de los caracteres.
+        /// 
+        /// Introduciremos la cadena 'hola qué tal' 
+        /// 
+        /// Nos deberá devolver 'lAtEUqAlOh'
+        /// </summary>
+        [TestMethod] 
+        public void transformaTextoTest()
+        {
+            string original = "hola qué tal";
+            string esperado = "lAtEUqAlOh";
+            string recibido = EjercicioCaracteres.transformaTexto(original);
+            Console.WriteLine("----- TEST transformaTexto. \nCadena entrada: " + original + "\nSalida esperada: " + esperado + "\nSalida REAL: " + recibido + "\n----- TEST FINALIZADO");
+            Assert.AreEqual(esperado, recibido);
+        }
+    }
+}
